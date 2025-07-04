743e730a04961fde2cd492c3b6a6333ab2d4ab66
Mercuraii <86083696+jszolcek@users.noreply.github.com>
2025-07-04 17:33:59 +0100

    Update script.js

--- script.js
+++ script.js
@@ -29,18 +29,18 @@
 
 //   if (bg) {
 // Initial opacity and transition settings
-bg.style.opacity = "0";
-bg.style.transition = "opacity 1s ease";
-bg.style.willChange = "opacity";
+// bg.style.opacity = "0";
+// bg.style.transition = "opacity 1s ease";
+// bg.style.willChange = "opacity";
 
-// Fade-in logic once video is ready
-const fadeIn = () => {
-  bg.style.opacity = "1";
-};
+// // Fade-in logic once video is ready
+// const fadeIn = () => {
+//   bg.style.opacity = "1";
+// };
 
-// Video might already be ready
-if (bg.readyState >= 3) {
-  fadeIn();
-} else {
-  bg.addEventListener("canplay", fadeIn);
-}
+// // Video might already be ready
+// if (bg.readyState >= 3) {
+//   fadeIn();
+// } else {
+//   bg.addEventListener("canplay", fadeIn);
+// }
